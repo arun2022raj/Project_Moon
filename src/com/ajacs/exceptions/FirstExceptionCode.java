d0c0a5cebeb6c9a5be662c54e6a39cd7c80f7007
Arun Rajesh <arunrajesh459@gmail.com>
2024-05-21 17:56:22 +0530

    exception day class 1 files uploaded

---
+++ src/com/ajacs/exceptions/FirstExceptionCode.java
@@ -0,0 +1,21 @@
+package com.ajacs.exceptions;
+
+public class FirstExceptionCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+		int i;
+		int j;
+		int k;
+		System.out.println(10/0);
+		int y;
+		int z;
+		} catch(Exception e) {
+			
+		}
+		
+	}
+
+}
