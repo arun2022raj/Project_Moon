6ebd8a07190817c8a54901f32262e1d50c930821
Arun Rajesh <arunrajesh459@gmail.com>
2024-05-30 14:10:54 +0530

    branch verification on changes

--- src/com/ajacs/arrayprograms2305/programs2705/UnwantedCharString.java
+++ src/com/ajacs/arrayprograms2305/programs2705/UnwantedCharString.java
@@ -1,10 +1,18 @@
 package com.ajacs.arrayprograms2305.programs2705;
 
+import java.util.Arrays;
+
 public class UnwantedCharString {
 	private static String[] name(String[] arr,String regex) {
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=arr[i].replaceAll(regex, "");
+		}
+		return arr;
 		
 	}
 	public static void main(String[] args) {
+		String[] arrs= {"arun123","rajesh456","jakkam789"};
+		System.out.println(Arrays.toString(name(arrs,"[j-k]")));
 		
 	}
 
